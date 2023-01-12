c8b097b62291bb47b19eb5906a5913a61423772b
stefank02 <107364691+stefank02@users.noreply.github.com>
2023-01-12 12:00:27 +0100

    Fixing the code

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,33 +1,20 @@
 import React from 'react';
+import './Avatar.css';
 
 const Avatar = ({ name, github, backgroundColor  }) => {
   const initials = name.split(' ').map(n => n[0]).slice(0, 2).join('');
 
-  const handleClick = () => {
-    window.location.href = `https://github.com/${github}`
-  }
-
   return (
-    <div
+    <a
     className="avatar"
     style={{
-        width: '75px',
-        height: '75px',
         backgroundColor: backgroundColor,
-        color: 'white',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontWeight: 'bold',
-        fontSize: '30px',
-        borderRadius: '50%',
-        cursor: 'pointer',
-        margin: '5px'
     }}
-    onClick={handleClick}
+    href = {`https://github.com/${github}`}
+    target = '_blank'
     >
     {initials}
-    </div>
+    </a>
   );
 }
 
