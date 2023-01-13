5a6d267b09191cd429260eddc905a4c931204acb
stefank02 <107364691+stefank02@users.noreply.github.com>
2023-01-13 11:03:33 +0100

    Settings Page First commit

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,7 +1,17 @@
-export default function SettingsPage() {
-  return (
-    <>
-      <h1>Settings Page</h1>
-    </>
-  );
+import React from 'react';
+import "./SettingPage.css"
+import Switch from '@mui/material/Switch';
+
+const Block = () => {
+    return (
+        <div className="Block">
+            <p id='Intro'>Settings</p>
+            <Switch id='SliderOne'>Button 1</Switch>
+            <p> Settings </p>
+            <br />
+            <Switch id='SliderTwo'>Button 2</Switch>
+        </div>
+    )
 }
+
+export default Block;
