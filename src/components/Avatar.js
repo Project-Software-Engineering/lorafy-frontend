fc5056f527194859073577bb7d63d15d9f5887ed
stijnstroeve <stijnstroeve@hotmail.com>
2023-01-16 22:00:45 +0100

    Added GitHub avatars to about page.

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './Avatar.css';
 
-const Avatar = ({ name, github, backgroundColor  }) => {
-  const initials = name.split(' ').map(n => n[0]).slice(0, 2).join('');
+export default function Avatar({ name, github }) {
+  const initials = name
+    .split(' ')
+    .map((n) => n[0])
+    .slice(0, 2)
+    .join('');
 
   return (
     <a
-    className="avatar"
-    style={{
-        backgroundColor: backgroundColor,
-    }}
-    href = {`https://github.com/${github}`}
-    target = '_blank'
+      className="avatar"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(https://github.com/${github}.png)`,
+        backgroundSize: 'cover',
+      }}
+      href={`https://github.com/${github}`}
+      target="_blank"
+      rel="noreferrer"
     >
-    {initials}
+      {initials}
     </a>
   );
 }
-
-export default Avatar;
